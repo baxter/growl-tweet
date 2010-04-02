24d1368b1425f4989f5bc1de2fe8449dcd76a0c8
baxter <paul.boxley@gmail.com>
2010-04-02 08:59:41 +0100

    Wrapped the whole thing in a while(1) and added a sleep for 1 minute.

--- twitter.rb
+++ twitter.rb
@@ -3,17 +3,22 @@ require 'growl'
 
 filename = 'state.yml'
 
-File.open(filename,'r+') do |f|
-  state = YAML::load(f)
-  Twitter::Search.new("@" + state['name']).since(state['last_id']).each do |s|
-    Growl.notify do |n|
-      n.image        = Dir.pwd + '/twittericon.png'
-      n.title        = "Tweet from #{s.from_user}"
-      n.message      = s.text.gsub('&lt;','<').gsub('&gt;','>')
+while(1) do
+  File.open(filename,'r+') do |f|
+    state = YAML::load(f)
+    Twitter::Search.new("@" + state['name']).since(state['last_id']).sort do |a,b|
+      a.id <=> b.id
+    end.each do |s|
+      Growl.notify do |n|
+        n.image        = Dir.pwd + '/twittericon.png'
+        n.title        = "Tweet from #{s.from_user}"
+        n.message      = s.text.gsub('&lt;','<').gsub('&gt;','>')
+      end
+      state['last_id'] = s.id unless s.id < state['last_id']
     end
-    state['last_id'] = s.id unless s.id < state['last_id']
+    f.pos = 0
+    f.print YAML.dump(state)
+    f.truncate(f.pos)
   end
-  f.pos = 0
-  f.print YAML.dump(state)
-  f.truncate(f.pos)
+  sleep(60 * 1) # 1 minute
 end
