4401897db78c55208d72002b187d934e962b7f3b
baxter <paul.boxley@gmail.com>
2010-04-02 07:58:49 +0100

    Wrapped everything in a File.open block. Added icon.

--- twitter.rb
+++ twitter.rb
@@ -1,11 +1,19 @@
 require 'twitter'
 require 'growl'
 
-state = YAML.load_file('state.yml')
+filename = 'state.yml'
 
-Twitter::Search.new('@baxt3r').since('11427179802').each do |s|
-  Growl.notify do |n|
-    n.title = "Tweet from #{s.from_user}"
-    n.message = s.text
+File.open(filename,'r+') do |f|
+  state = YAML::load(f)
+  Twitter::Search.new("@" + state['name']).since(state['last_id']).each do |s|
+    Growl.notify do |n|
+      n.image        = Dir.pwd + '/twittericon.png'
+      n.title        = "Tweet from #{s.from_user}"
+      n.message      = s.text.gsub('&lt;','<').gsub('&gt;','>')
+    end
+    state['last_id'] = s.id unless s.id < state['last_id']
   end
+  f.pos = 0
+  f.print YAML.dump(state)
+  f.truncate(f.pos)
 end
