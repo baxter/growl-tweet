b3736ceaf54b56fc70759e0858a5e9a4551fc3c3
baxter <paul.boxley@gmail.com>
2010-04-03 10:31:25 +0100

    Download the user's Twitter image to use as the Growl image.

--- twitter.rb
+++ twitter.rb
@@ -1,7 +1,33 @@
+require 'net/http'
 require 'twitter'
 require 'growl'
 
 filename = 'state.yml'
+
+def profile_image(user, img_url)
+  if ! File.exist?(image_path(user)) || File.mtime(image_path(user)) < Time.at(60 * 60 * 24 * 7 * -1) # 7 days ago
+    download_image(user,img_url)
+  end
+  File.exist?(image_path(user)) ? image_path(user) : image_path(:default)
+end
+
+def image_path(user)
+  if user == :default
+    'profile_pics/default.png'
+  else
+    'profile_pics/' + user
+  end
+end
+
+def download_image(user, img_url)
+  begin
+    image = Net::HTTP.get(URI.parse(img_url))
+    f = File.new(image_path(user),'wb')
+    f.write(image)
+  ensure
+    f.close unless f.nil?
+  end
+end
 
 while(1) do
   File.open(filename,'r+') do |f|
@@ -10,9 +36,9 @@ while(1) do
       a.id <=> b.id
     end.each do |s|
       Growl.notify do |n|
-        n.image        = Dir.pwd + '/twittericon.png'
-        n.title        = "Tweet from #{s.from_user}"
-        n.message      = s.text.gsub('&lt;','<').gsub('&gt;','>')
+        n.image         = profile_image(s.from_user, s.profile_image_url)
+        n.title         = "Tweet from #{s.from_user}"
+        n.message       = s.text.gsub('&lt;','<').gsub('&gt;','>')
       end
       state['last_id'] = s.id unless s.id < state['last_id']
     end
